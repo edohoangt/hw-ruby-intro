632db4a7e9dc3ac7f0765940d450f4f3b5edfbc8
Hoang Tran <descience.thh10@gmail.com>
2023-02-09 11:22:08 +0700

    finish homework

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,95 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+    res = 0
+    arr.each do |elem| res += elem end
+    return res
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+    sorted_arr = arr.sort
+    sorted_arr.reverse!
+
+    len = arr.length()
+    if len == 0
+        return 0
+    elsif len == 1
+        return arr[0]
+    end
+
+    return sorted_arr[0] + sorted_arr[1]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+    if arr.length() == 0
+        return false
+    end
+    
+    st = Hash[]
+    arr.each do |elem| 
+        if st.has_key?(n - elem)
+            return true
+        end
+
+        st[elem] = 1
+    end
+
+    return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+    return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+    if s.length() == 0 || !(s =~ /^[a-zA-Z]+$/)
+        return false
+    end
+
+    vowels = "ueoaiUEOAI"
+    return !vowels.include?(s[0])
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    return (s =~ /^(0|1)+(00)$/) || s == "0"
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    def initialize(isbn, price)
+        if isbn.length() == 0 || price <= 0
+            raise ArgumentError.new("invalid isbn or price!")
+        end
+
+        @isbn, @price = isbn, price
+    end
+
+    def isbn
+        @isbn
+    end
+
+    def price
+        @price
+    end
+
+    def isbn=(val)
+        if isbn.length() == 0
+            raise ArgumentError.new("invalid isbn!")
+        end
+        @isbn = val
+    end
+
+    def price=(val)
+        if price <=  0
+            raise ArgumentError.new("invalid price!")
+        end
+        @price = val
+    end
+
+    def price_as_string
+        return "$%0.2f" % [@price]
+    end
 end
